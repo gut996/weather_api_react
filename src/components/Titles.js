0d96147461a02690fa0061a1c5ea2e66d7ca1e05
gut996 <gutu.ion.00@gmail.com>
2020-10-21 10:28:45 -0700

    GitHub link added

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,6 +4,8 @@ const Titles =() => (
     <div>
                 <h1 className="title-container__title">Weather Finder</h1>
                 <h3 className="title-container__subtitle">Check the weather of your city</h3>
+                <button><a target="_blank" className="title-container-button_link" href="https://github.com/gut996/weather_api_react">GitHub Code</a></button>
+
             </div>
 );
 
