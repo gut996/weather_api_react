b5a5e4b5d9693457f0b74114e9ed5bca0b92b657
gut996 <gutu.ion.00@gmail.com>
2020-10-21 10:02:18 -0700

    test https

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
     );
     // const api_call2 = await fetch(
     //   `    https://www.amdoren.com/api/timezone.php?api_key=${API_KEY2}&loc=New+York
